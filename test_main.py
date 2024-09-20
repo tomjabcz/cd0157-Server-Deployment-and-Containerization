4fdf3d1b93a5efd2c756f07da10b8b5d44bda452
Tomas Jaburek <tomas.jaburek@gmail.com>
2024-09-21 01:00:48 +0200

    with Assert False

--- test_main.py
+++ test_main.py
@@ -24,6 +24,8 @@ def client():
 
 def test_health(client):
     response = client.get('/')
+    # for testing purposes
+    assert False
     assert response.status_code == 200
     assert response.json == 'Healthy'
 
